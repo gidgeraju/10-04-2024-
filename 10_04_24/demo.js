ac630dfceebc2199baa25b3a6822ca99172383eb
gidgeraju <142143044+gidgeraju@users.noreply.github.com>
2024-04-10 18:03:39 +0530

    Add files via upload

---
+++ 10_04_24/demo.js
@@ -0,0 +1,44 @@
+// task1
+let dateToday = new Date();
+let day = dateToday.getDate();
+console.log(day);
+let year = dateToday.getFullYear();
+console.log(year);
+let month = dateToday.getMonth();
+console.log(month);
+let seconds =dateToday.getMilliseconds();
+console.log(seconds);
+let second = dateToday.getSeconds();
+console.log(second);
+
+// task2
+let mathematical = Math;
+let park = 3.14;
+let ceilValue = mathematical.ceil(park);
+console.log(ceilValue);
+let floorValue = mathematical.floor(park);
+console.log(floorValue);
+let roundValue = mathematical.round(park);
+console.log(roundValue);
+
+// task3
+let Num1 = 60;
+let Num2 = 40;
+let max1 = mathematical.min(Num1,Num2);
+console.log(max1);
+let min1 = mathematical.max(Num1,Num2);
+console.log(min1);
+let Num3=25;
+let square = mathematical.sqrt(Num3);
+console.log(square);
+let a = 78.5
+b = mathematical.round(a);
+console.log(b);
+c = mathematical.floor(a);
+console.log(c);
+d = mathematical.round(a);
+console.log(d);
+let you = mathematical.random();
+your = you*1000;
+yours = mathematical.floor(your);
+console.log(yours);
